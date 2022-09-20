8ae66ec157a67b33e1808358f87cf5bda9c930eb
blalyasar <49458946+blalyasar@users.noreply.github.com>
2022-09-20 10:58:47 +0300

    Update bash_for_loop.sh

--- bash_for_loop.sh
+++ bash_for_loop.sh
@@ -1,1 +1,4 @@
+# dosyadan tek satır okuyup python dosyasına parametre ile verilip çıktıları yeni dosyaya yazma
+for i in $(cat dosya.txt);do pythondosyasi.py $i ; done > yenidosya.txt
+
 for i in `ls -1 *.sh`; do  echo $i; done
