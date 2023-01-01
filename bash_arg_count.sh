d47c36aa710d26af80632632b9b883bc01098205
blalyasar <49458946+blalyasar@users.noreply.github.com>
2023-01-01 23:25:23 +0300

    Update bash_arg_count.sh

--- bash_arg_count.sh
+++ bash_arg_count.sh
@@ -10,4 +10,8 @@ if [ "$#" == "0" ]; then
     echo "no arguments"
 else
     echo  "number of argument is $#"
+    echo  "argument 1 :  is $1"
+
+    # 1DEN FAZLA ARGUMANIN KONTROLU YOK 2. BOS ISE BOS YAZAR
+    echo  "argument 2 :  is $2" 
 fi
