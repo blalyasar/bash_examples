cc80d77a25c68bd27b7c890939acfe80325f53cf
blalyasar <49458946+blalyasar@users.noreply.github.com>
2024-01-14 15:47:07 +0300

    Update bash_for_loop.sh
    multiple rename

--- bash_for_loop.sh
+++ bash_for_loop.sh
@@ -2,3 +2,14 @@
 for i in $(cat dosya.txt);do pythondosyasi.py $i ; done > yenidosya.txt
 
 for i in `ls -1 *.sh`; do  echo $i; done
+
+
+# touch {1..200}.mp4
+
+filenumber=0
+for file in  *.mp4
+do
+  echo $file
+  filenumber=$(($filenumber+1))
+  mv $file "S1.E"$filenumber
+done
